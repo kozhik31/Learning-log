4b915abcdf75f82008226b8ead14e648ba6e1b95
GLEB <govnozalypa321@gmail.com>
2025-07-23 15:09:38 +0300

    add feature registration, log in, log out access denied to other users topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -10,21 +12,26 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required()
 def topics(request):
     """Страница со списком тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required()
 def topic(request, topic_id):
     """Страница с одной темой и всеми записями"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required()
 def new_topic(request):
     """Страница с новой темой"""
     if request.method != 'POST':
@@ -32,16 +39,20 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required()
 def new_entry(request, topic_id):
     """Страница с новой записью по конкретной теме"""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         form = EntryForm()
@@ -56,10 +67,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+
+@login_required()
 def edit_entry(request, entry_id):
     """Страница с редактированием записи"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -71,3 +85,8 @@ def edit_entry(request, entry_id):
 
     context = {'topic': topic, 'form': form, 'entry': entry}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(topic, request):
+    if topic.owner != request.user:
+        raise Http404
